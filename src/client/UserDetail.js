e6042ee945f5f84298a64d76f961359d0c7ad089
Pavel Poláček <polacekpav@gmail.com>
2016-10-25 08:07:38 +0200

    step1

--- src/client/UserDetail.js
+++ src/client/UserDetail.js
@@ -11,13 +11,30 @@ class UserDetail extends Component {
                 <h3>{this.props.user.firstName} {this.props.user.lastName} {this.props.user.github && (this.props.user.github.username)} </h3>
             </div>
             <div>
-                <img src="https://avatars0.githubusercontent.com/u/273551?v=3&s=140" width={50} height={50} />
+                <img src="https://avatars0.githubusercontent.com/u/273551?v=3&s=140" width={50} height={50}/>
             </div>
             <div>
                 <p>Prague</p>
             </div>
             <table className="eventTable">
-
+                <table className="eventTable">
+                    <tr>
+                        <th>
+                            Event type
+                        </th>
+                        <th>
+                            Weather
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>Fork</td>
+                        <td><img src={`weather/clear-day.png`} width={24} height={24}/></td>
+                    </tr>
+                    <tr>
+                        <td>Watch</td>
+                        <td><img src={`weather/clear-day.png`} width={24} height={24}/></td>
+                    </tr>
+                </table>
             </table>
 
         </div>;
